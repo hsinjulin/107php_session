3cf1dd1415b5d8c0211f55e6fd966d2faa462533
LINHSINJU <11010148@gm.chihlee.edu.tw>
2023-09-21 11:46:33 +0800

    設定首頁樣式

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>107期末測驗實作題</title>
+</head>
+<body>
+    <form action="myPHP.php" id="form1" name="form1">
+        <input type="submit" name="submit" id="submit" value="送出">
+    </form>
+</body>
+</html>
